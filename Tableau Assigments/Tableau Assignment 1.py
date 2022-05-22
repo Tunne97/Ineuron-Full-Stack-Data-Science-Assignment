0833e285f843da85ee3e7314bb4e11867edf9a01
Tunnie <94374583+Tunne97@users.noreply.github.com>
2022-05-22 17:35:40 +0700

    Add files via upload

---
+++ Tableau Assigments/Tableau Assignment 1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tableau Assignment 1
+
+# `workbook link` : https://public.tableau.com/views/iNeuronTableauAssignment1-InsideSalesDS/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link
+
+# ## Show largest selling products and also show what % of sales is coming from which regions.
+
+# <img src="downloads/sales by region.png" width="400" height="100">
+
+# ## Create a side-by-side bar to compare Actual vs Target sales of different products.
+
+# <img src="downloads/Actual sales vs target sales.png" width="400" height="100">
+
+# ## Create a tree map to show largest selling regions.
+# 
+
+# <img src="downloads/Tree map - largest selling regions.png" width="400" height="100">
+
+# ## Create a packed bubble to show product sales.
+# 
+
+# <img src="downloads/Bubble chart- Product sales.png" width="400" height="100">
+
+# ## Create a side by side circle chart to compare actual vs target sales of different products.
+
+# <img src="downloads/side-by-side circle chart- actual vs target sales.png" width="400" height="100">
+
+# In[ ]:
+
+
+
+
